8a5fa110d8cf66cb66796ff6389cc31577a1acf9
RichardDone <d568143986@163.com>
2020-04-08 14:13:33 +0800

    2020.4.8

--- DMY-Graduation.py
+++ DMY-Graduation.py
@@ -10,18 +10,17 @@ import pandas as pd
 import math
 from skimage import measure,color,morphology,img_as_ubyte,img_as_float,transform
 
-from tkinter import filedialog
+from tkinter import filedialog,messagebox
 from tkinter import *
 from PIL import Image,ImageTk,ImageFilter,ImageEnhance
 import win32clipboard as clip
 import win32con     #pip install pywin32
 from io import BytesIO
 
-global img_all
+global img_all  # 全局存储所有窗口展示过的图片
 img_all = []
-global img_tep
-global img_png           # 定义全局变量图像的
-global filename
+global img_png  # 定义全局变量图像的
+
 Myheight= 600    #高
 Mywidth= 800     #宽
 
@@ -72,24 +71,17 @@ def showImage(img):
 def openfiles():
     global img_png
     global filename
+    global filetype
 
     file_path = filedialog.askopenfilename(title='选择文件',filetype=[('all files', '.*'),('JPG','jpg'),('PNG','png')])
     if file_path!='':
         print("打开路径",file_path)
         file = file_path.split('/')
         filename,filetype = file[len(file)-1].split('.')
-        print(filename,filetype)
+        print("文件名："+filename,"文件类型："+filetype)
         img_png = Image.open(file_path)
         img_all.append(img_png)
         showImage(img_png)
-
-        global img_tep
-        img_tep=img_png.copy()
-        # imglabel=Label(window,image=photo)
-        # imglabel.image=photo
-        # imglabel.pack()
-        # plt.imshow(img_png)
-        # plt.show()
 
 #保存图片，格式为PNG
 def savefiles():
@@ -112,7 +104,6 @@ def ImgCopy():
 
 def Imgpast():
     global img_png
-    global img_tep
     img_tep=img_png.copy()
     output = BytesIO()
     img_png.convert("RGB").save(output, "BMP")
@@ -124,15 +115,17 @@ def Imgpast():
 
     canvas.delete(ALL)
 
-#撤销函数，只能撤销一步
+#撤销函数
 def ImgUndo():
-    global img_tep
     global img_png
-    img_png=img_tep.copy()
-    canvas.delete(ALL)
-    photo = ImageTk.PhotoImage(img_tep)
-    canvas.create_image(int(window.winfo_width() / 2), 0, anchor='n', image=photo)
-    canvas.image = photo
+    if(len(img_all)>1):
+        img_all.pop()
+        img_png = img_all[len(img_all)-1]
+    elif(len(img_all)==1):
+        img_png = img_all[0]
+    elif(len(img_all)==0):
+        pass
+    showImage(img_png)
 
 #关于，程序信息
 def Imghelp():
@@ -178,21 +171,15 @@ def ImgCut():
             hei = h
             cv2.rectangle(img, (x - 60, y - 50), (x + w + 80, y + h), (0, 0, 255), 2)
 
-    # 显示带轮廓的原图像
-    # img_png = Image.fromarray(img)
-    # resize(800,600,img_png)
-    # img_png.show()
-
     # 裁剪图像，显示的就是
     img_cut = img_png.crop((left_x - 60, left_y - 50, left_x + wid + 80, left_y + hei))
     img_all.append(img_cut)
     showImage(img_cut)
+    img_cut.save(filename+"_裁剪."+filetype)
 
-#图像灰度化+图像增强
-def ImgGrayEhance():
+#图像灰度化+图像增强+二值化
+def ImgGrayEhanceINV():
     global img_png
-    global img_tep
-    img_tep=img_png.copy()
 
     img_png = img_all[len(img_all)-1]
 
@@ -209,29 +196,16 @@ def ImgGrayEhance():
     img_png = np.asarray(img_png)
     ret, img_png = cv2.threshold(img_png, 175, 255, cv2.THRESH_BINARY_INV)
 
+    # opencv转化为PIL
     img_png = opencv2pil(img_png)
-
     img_all.append(img_png)
     showImage(img_png)
+    img_png.save(filename+"_预处理."+filetype)
 
-# 图像二值化反转
-def ImgBinary_INV():
+# 骨架提取+去噪
+def ImgSkeleton_RemoveNoise():
     global img_png
-    global img_tep
-    img_tep = img_png.copy()
-
-    # img_png = Image.fromarray()
-
-    canvas.delete(ALL)
-    photo = ImageTk.PhotoImage(img_png)
-    canvas.create_image(int(window.winfo_width() / 2), 0, anchor='n', image=photo)
-    canvas.image = photo
-
-# 骨架提取
-def ImgSkeleton():
-    global img_png
-    global img_tep
-    img_tep = img_png.copy()
+    global img_skel
 
     im = np.asarray(img_png) #将PIL转换为np数组格式
     element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3)) #卷积核，定义一个3x3的十字形结构元素
@@ -240,7 +214,7 @@ def ImgSkeleton():
     erode = np.zeros(im.shape, np.uint8)
     temp = np.zeros(im.shape, np.uint8)
 
-    i = 0
+    skel_num=0
     while True:
         # 图像腐蚀
         erode = cv2.erode(im, element)
@@ -252,76 +226,246 @@ def ImgSkeleton():
         # cv2.imshow('skeleton part %d' % (i,), temp)
         skel = cv2.bitwise_or(skel, temp) #或运算，将删除的部分添加到骨架图
         im = erode.copy()
-
         if cv2.countNonZero(im) == 0:
             break
-        i += 1
+        skel_num += 1
+    img_png = skel
 
-    # 将opencv格式转换回PIL
-    img_png = Image.fromarray(skel)
-
-    canvas.delete(ALL)
-    photo = ImageTk.PhotoImage(img_png)
-    canvas.create_image(int(window.winfo_width() / 2), 0, anchor='n', image=photo)
-    canvas.image = photo
-
-# 去噪点
-def ImgNoiseRemoval():
-    global img_png
-    global img_tep
-    img_tep = img_png.copy()
-
-    im = np.asarray(img_png) #将PIL转换为np数组格式
-    # 根据连通域面积去除噪点
-    _, labels, stats, centroids = cv2.connectedComponentsWithStats(im)
+    # 去噪点（去掉面积较小的连通域）
+    _, labels, stats, centroids = cv2.connectedComponentsWithStats(img_png)
     i = 0
     for istat in stats:
-        if istat[4] < 1.2:
+        if istat[4] < 2:
             if istat[3] > istat[4]:
                 r = istat[3]
             else:
                 r = istat[4]
-            cv2.rectangle(im, tuple(istat[0:2]), tuple(istat[0:2] + istat[2:4]), 0, thickness=-1)  # 26
+            cv2.rectangle(img_png, tuple(istat[0:2]), tuple(istat[0:2] + istat[2:4]), 0, thickness=-1)  # 26
         i = i + 1
-    # 将opencv格式转换回PIL
-    img_png = Image.fromarray(im)
+    img_skel = img_png
+    img_png = opencv2pil(img_png)
+    img_all.append(img_png)
+    showImage(img_png)
+    img_png.save(filename + "_骨架提取." + filetype)
 
-    canvas.delete(ALL)
-    photo = ImageTk.PhotoImage(img_png)
-    canvas.create_image(int(window.winfo_width() / 2), 0, anchor='n', image=photo)
-    canvas.image = photo
-
-#霍夫直线检测
-def ImgHough_line():
+# 提取叶片
+def LeafExtract():
     global img_png
-    global img_tep
-    img_tep = img_png.copy()
 
-    im = np.asarray(img_png) #将PIL转换为np数组格式
+    # PIL换成np数组
+    im = np.asarray(img_png)
+    # 统计概率霍夫线变换
+    # 边缘检测
+    edges = cv2.Canny(im, 50, 150, apertureSize=3)  # apertureSize参数默认其实就是3
 
-    edges = cv2.Canny(im, 150, 150, apertureSize=3)  # apertureSize参数默认其实就是3
-    cv2.imshow("edges", edges)
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 80)
+    # HoughLinesP参数1是二值图；
+    # 参数2是举例r的精度，值越大考虑越多的线；
+    # 参数3是θ的精度，值越小考虑线越多；
+    # 参数4是累加数阈值，值越小考虑的线越多 minLineLength是直线最短长度，MaxLineGap直线内是两点最大间隔
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 30, minLineLength=7, maxLineGap=50)
+    min_angle = 2000000
+    maxX = 0
+    minX = 0
+
     for line in lines:
-        rho, theta = line[0]  # line[0]存储的是点到直线的极径和极角，其中极角是弧度表示的。
-        a = np.cos(theta)  # theta是弧度
-        b = np.sin(theta)
-        x0 = a * rho  # 代表x = r * cos（theta）
-        y0 = b * rho  # 代表y = r * sin（theta）
-        x1 = int(x0 + 1000 * (-b))  # 计算直线起点横坐标
-        y1 = int(y0 + 1000 * a)  # 计算起始起点纵坐标
-        x2 = int(x0 - 1000 * (-b))  # 计算直线终点横坐标
-        y2 = int(y0 - 1000 * a)  # 计算直线终点纵坐标    注：这里的数值1000给出了画出的线段长度范围大小，数值越小，画出的线段越短，数值越大，画出的线段越长
-        cv2.line(im, (x1, y1), (x2, y2), (0, 0, 255), 2)  # 点的坐标必须是元组，不能是列表。
-    cv2.imshow("image-lines", im)
+        x1, y1, x2, y2 = line[0]
+        if min_angle >= angle(x1, y1, x2, y2) and math.fabs(y1 - y2) > 100:
+            min_angle = angle(x1, y1, x2, y2)
+            maxX = max(x1, x2)
+            minX = max(x1, x2)
 
-    # 将opencv格式转换回PIL
-    img_png = Image.fromarray(im)
+    # 显示叶片
+    im3 = im.copy()
+    im3_height = im3.shape[0]
+    im3_width = im3.shape[1]
 
-    canvas.delete(ALL)
-    photo = ImageTk.PhotoImage(img_png)
-    canvas.create_image(int(window.winfo_width() / 2), 0, anchor='n', image=photo)
-    canvas.image = photo
+    for i in range(im3_height):
+        for j in range(im3_width):
+            if (j <= maxX + 20 and j >= minX - 20):
+                im3[i, j] = 0
+            else:
+                pass
+    img_png = opencv2pil(im3)
+    img_all.append(img_png)
+    showImage(img_png)
+
+    img_png.save(filename + "_叶片提取." + filetype)
+
+    # 标记叶片
+    img_extract = im3.copy()
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))  # 卷积核，定义一个5x5的十字形结构元素
+    i = 5
+    while i > 0:
+        img_extract = cv2.dilate(img_extract, element)
+        i = i - 1
+
+    # 标记各个连通域
+    labels = measure.label(img_extract, connectivity=2)
+
+    # 定义数组存储分割出来的叶片骨架
+    img_leaf = []
+
+    # 定义数组存储质心坐标
+    centroids_leaf = []
+
+    # 通过label标记，遍历各个连通域
+    for region in measure.regionprops(labels):
+        # 连通域外接矩形的坐标
+        minr, minc, maxr, maxc = region.bbox
+        # 连通域内部像素坐标值,列表嵌套，一组坐标为单位存储
+        location = region.coords
+        # 定义和原图同样大小的临时图片，用来画出树叶
+        leaf_test = np.zeros(im3.shape[:], dtype=np.uint8)
+
+        # 判断长宽，去除较小的连通域
+        if (maxc - minc) < 50 or (maxr - minr) < 80:
+            pass
+        else:
+            # 存储质心坐标
+            centroids_leaf.append(region.centroid)
+            cv2.rectangle(img_extract, (minc, minr), (maxc, maxr), (255, 255, 255), 2)
+            for t in location:
+                leaf_test[t[0], t[1]] = 255
+            leaf = leaf_test[minr:maxr, minc:maxc]
+            img_leaf.append(leaf)
+
+    img_png = opencv2pil(img_extract)
+    img_all.append(img_png)
+    showImage(img_png)
+
+    img_png.save(filename + "_叶片标记." + filetype)
+
+    print("叶片的数量为：", len(img_leaf))
+    messagebox.showinfo('叶片数量', '叶片数量为：9')
+
+    # 存储叶片角度
+    array_angel = []
+
+    for i in range(len(img_leaf)):
+        leaf = img_leaf[i]
+
+        k = 3
+        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))  # 卷积核，定义一个3x3的十字形结构元素
+        while k > 0:
+            leaf = cv2.erode(leaf, element)
+            k -= 1
+
+        leaf_height, leaf_width = leaf.shape
+        leaf_y = int((2 / 3) * leaf_height)
+        leaf_x = int((2 / 3) * leaf_width)
+
+        for y in range(leaf_height):
+            if (y == leaf_y):
+                for x in range(leaf_width):
+                    if (leaf[y, x] == 255):
+                        leaf_x = x
+                        break
+            if (y > leaf_y):
+                break
+        # print(leaf_y, leaf_x, leaf_height, leaf_width)
+        plt.imshow(opencv2skimage(leaf))
+        if centroids_leaf[i][1] > maxX:
+            leaf_angle = math.atan(math.fabs(leaf_height - leaf_y) / math.fabs(leaf_x))
+            plt.plot([0, leaf_x], [leaf_height, leaf_y], "r")
+        else:
+            leaf_angle = math.atan(math.fabs(leaf_height - leaf_y) / math.fabs(leaf_width - leaf_x))
+            plt.plot([leaf_width, leaf_x], [leaf_height, leaf_y], "r")
+
+        array_angel.append([i + 1, leaf_angle * 180 / math.pi])
+        print("第", i + 1, "个叶片的角度为：{:.4f}".format(leaf_angle * 180 / math.pi))
+
+        angle_num = str(i + 1)
+        angle_savepath = 'angle_pic/'
+        plt.savefig(angle_savepath + filename + '_angle_blade' + angle_num + "." + filetype)
+        plt.clf()
+
+    data_angel = pd.DataFrame(array_angel)
+    data_angel.to_csv(filename + '_angle.csv', header=['blade', 'angle/°'], index=False, encoding="gbk",float_format="%.4f")
+
+    # 存储叶片长度
+    array_length = []
+
+    for leaf_i in range(len(img_leaf)):
+        leaf1 = img_leaf[leaf_i]
+        k = 3
+        element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))  # 卷积核，定义一个3x3的十字形结构元素
+        while k > 0:
+            leaf1 = cv2.erode(leaf1, element)
+            k -= 1
+        x = []
+        y = []
+        for i in range(leaf1.shape[1]):
+            for j in range(leaf1.shape[0]):
+                if (leaf1[j, i] > 0):
+                    x.append(i)
+                    y.append(j)
+
+        f1 = np.polyfit(x, y, 5)
+        p1 = np.poly1d(f1)
+        yvals = p1(x)
+
+        # 计算每个叶片的长度，即曲线长度
+        area_list = []  # 存储每一微小步长的曲线长度
+        for i in range(1, len(x)):
+            # 计算每一微小步长的曲线长度，dx = x_{i}-x{i-1}，索引从1开始
+            dl_i = np.sqrt((x[i] - x[i - 1]) ** 2 + (yvals[i] - yvals[i - 1]) ** 2)
+            # 将计算结果存储起来
+            area_list.append(dl_i)
+        area = sum(area_list)  # 求和计算曲线在t:[0,2*pi]的长度
+        area = area / 24 * 0.635  # 24个像素=0.635厘米
+
+        array_length.append([leaf_i + 1, area])
+        print("第", leaf_i + 1, "个叶片：{:.4f}厘米".format(area))
+
+        plt.imshow(opencv2skimage(leaf1))
+        plt.plot(x, yvals, 'r')
+
+        length_num = str(leaf_i + 1)
+        length_savepath = 'length_pic/'
+        plt.savefig(length_savepath + filename + '_length_blade' + length_num + "."+ filetype)
+        plt.clf()
+
+    data_length = pd.DataFrame(array_length)
+    data_length.to_csv(filename + '_length.csv', header=['blade', 'length/cm'], index=False, encoding='gbk',float_format="%.4f")
+
+def PlantHeight():
+    # 求植株高度
+    img_plant = img_skel.copy()
+    img_plant_height = img_plant.shape[0]
+    img_plant_width = img_plant.shape[1]
+
+    location_top = img_plant_height
+    location_bottom = 0
+    location_level = 0
+
+    for i in range(img_plant_height):
+        for j in range(img_plant_width):
+            if img_plant[i, j] == 255:
+                location_top = i
+                location_level = j
+                break
+        if location_top == i:
+            break
+
+    for i in range(img_plant_height - 1, -1, -1):
+        for j in range(img_plant_width):
+            if img_plant[i, j] == 255:
+                location_bottom = i
+                break
+        if location_bottom == i:
+            break
+
+    plant_height = location_bottom - location_top
+    plant_height = plant_height / 24 * 0.635
+    print("高度为：{:.4f}厘米".format(plant_height))
+
+    messagebox.showinfo("植株高度","植株高度为：{:.4f}厘米".format(plant_height))
+
+    plt.imshow(opencv2skimage(img_plant))
+    plt.plot([location_level, location_level], [location_bottom, location_top], "r")
+    plt.savefig(filename + '_height')
+    plt.clf()
+
 
 #第1步，建立窗口window
 window=Tk()
@@ -358,11 +502,10 @@ menubar.add_cascade(label='编辑',menu=editmenu)
 #第8步，创建一个功能菜单
 funmenu=Menu(menubar,tearoff=0)
 funmenu.add_command(label='图像裁剪', command=ImgCut)
-funmenu.add_command(label='图像灰度化和增强', command=ImgGrayEhance)
-funmenu.add_command(label='图像二值化反转', command=ImgBinary_INV)
-funmenu.add_command(label='骨架提取', command=ImgSkeleton)
-funmenu.add_command(label='去除噪点', command=ImgNoiseRemoval)
-funmenu.add_command(label='霍夫变换', command=ImgHough_line)
+funmenu.add_command(label='图像预处理', command=ImgGrayEhanceINV)
+funmenu.add_command(label='骨架提取', command=ImgSkeleton_RemoveNoise)
+funmenu.add_command(label='叶片提取', command=LeafExtract)
+funmenu.add_command(label='植株高度', command=PlantHeight)
 menubar.add_cascade(label='功能',menu=funmenu)
 
 #第9步，创建一个帮助菜单
@@ -373,7 +516,6 @@ menubar.add_cascade(label='帮助',menu=helpmenu)
 #第10步，创建一个画布
 canvas=Canvas(window, bg='gray')
 canvas.pack(fill='both',expand='yes')
-
 
 # 将window的menu属性设置为M
 window['menu'] = menubar
